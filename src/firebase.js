7467b038c9cd9d299a81e0f64ea17bd8f1532ba0
Adarsh Jha <adarshjhaxif@gmail.com>
2023-11-13 17:16:03 +0530

    Homepage, Products and basic Checkout page done.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAmBoxYbBR5RaJDyXlfxa-b271i56XZxTE",
+    authDomain: "clone-a7a8c.firebaseapp.com",
+    projectId: "clone-a7a8c",
+    storageBucket: "clone-a7a8c.appspot.com",
+    messagingSenderId: "1097190003085",
+    appId: "1:1097190003085:web:1e3ff1fb81d95ebd426dc4",
+    measurementId: "G-KRM866LJLK"
+  };
